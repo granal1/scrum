49edb181466c94cdef829d8e8db7daa0e4468128
Jhon Mosk <jhon-mosk@yandex.ru>
2022-12-16 22:04:11 +0300

    подключил конфиг

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
-session_start();
+include '../vendor/autoload.php';
 
 require $_SERVER['DOCUMENT_ROOT'] . "/../config/config.php";
 
 require_once ROOT . '/model/User.php';
 require_once ROOT . '/model/Task.php';
-
+session_start();
 
 $controller = $_GET['controller'] ?? 'index';
 
-$routes = require ROOT . '/routes.php';
+$routes = require ROOT . '/routes/routes.php';
 
 if (isset($_SESSION['user'])) {
     require_once $routes[$controller] ?? require_once $routes['index']; // вместо 404 просто переход на главную (всплвыающее уведомление не делал)
