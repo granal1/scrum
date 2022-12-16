93ea73108213872a43aad088b66cf6052e184c04
Jhon Mosk <jhon-mosk@yandex.ru>
2022-12-16 21:58:54 +0300

    переместил роуты в папку routes

--- routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'index' => ROOT . '/controller/IndexController.php',
-    'security' => ROOT . '/controller/SecurityController.php',
-    'tasks' => ROOT . '/controller/TasksController.php',
-    'taskCard' => ROOT . '/controller/TasksCardController.php',
-];
---
+++ routes/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'index' => ROOT . '/controller/IndexController.php',
+    'security' => ROOT . '/controller/SecurityController.php',
+    'tasks' => ROOT . '/controller/TasksController.php',
+    'taskCard' => ROOT . '/controller/TasksCardController.php',
+];
