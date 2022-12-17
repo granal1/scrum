61bf113bebf15db9682f1498a0ed042983da86e8
Jhon Mosk <jhon-mosk@yandex.ru>
2022-12-17 13:32:45 +0300

    роуты теперь находяться в папке routes, перенёс новые роуты

--- routes/routes.php
+++ routes/routes.php
@@ -5,4 +5,7 @@ return [
     'security' => ROOT . '/controller/SecurityController.php',
     'tasks' => ROOT . '/controller/TasksController.php',
     'taskCard' => ROOT . '/controller/TasksCardController.php',
+    'file_upload' => ROOT . 'controller/FileUploadController.php',
+    'files' => ROOT . 'controller/FileController.php',
+    'profile' => ROOT . 'controller/ProfileController.php',
 ];
