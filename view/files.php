2c8a1959502dd4aec2d072efc8a070e5b1c94320
granal1 <granal1@mail.ru>
2022-12-17 21:44:14 +0200

    Исправлен путь к bootstrap

--- view/files.php
+++ view/files.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Профиль</title>
-    <link href="../bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
--- view/register.php
+++ view/register.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Регистрация</title>
-    <link href="../bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/bootstrap.min.css" rel="stylesheet">
     <!-- <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet"
