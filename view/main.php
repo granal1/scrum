93b09a84ee0f3052e26bfcc49c005917ba609be0
Jhon Mosk <jhon-mosk@yandex.ru>
2022-12-17 12:08:50 +0300

    подключил функцию percent

--- view/main.php
+++ view/main.php
@@ -1,4 +1,3 @@
-
 <h1>Рады вас приветствовать, <?= $username ?></h1>
 
 <a href="/?controller=taskCard" class="btn btn-lg btn-primary mt-1">Добавить задачу</a>
@@ -16,14 +15,18 @@
   </thead>
   <tbody>
 
-<?php foreach ($_SESSION['tasks'] as $key => $task):?>
-    <tr style="cursor: pointer"; onclick="window.location.href='/?controller=taskCard&action=<?=$key?>'; return false">
-      <td align="center"><?=$task->getTask_priority()?></td>
-      <td><?=$task->getTask_description()?></td>
-      <td align="center"><?=date('d.m.Y H:i', strtotime($task->getTask_deadline()))?></td> 
-      <td align="center"><?=percent($task->getTask_done())?></td> 
-    </tr>
-<?php endforeach;?>
+    <?php
+
+    use function app\functions\percent;
+
+    foreach ($_SESSION['tasks'] as $key => $task) : ?>
+      <tr style="cursor: pointer" ; onclick="window.location.href='/?controller=taskCard&action=<?= $key ?>'; return false">
+        <td align="center"><?= $task->getTask_priority() ?></td>
+        <td><?= $task->getTask_description() ?></td>
+        <td align="center"><?= date('d.m.Y H:i', strtotime($task->getTask_deadline())) ?></td>
+        <td align="center"><?= percent($task->getTask_done()) ?></td>
+      </tr>
+    <?php endforeach; ?>
 
   </tbody>
 </table>
